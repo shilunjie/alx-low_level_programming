3894bfdb1f4f129499076d78cdf16005024945f3
Shilunjie <lshilunji@gmail.com>
2023-06-20 13:29:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print out _putchar using printf
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/* Passing arguements to _putchar method */
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}

f14ab30cb2370339ff9ec16ef38061b5c67eb106
Shilunjie <lshilunji@gmail.com>
2023-06-20 17:01:10 -0700

    files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -19,7 +19,7 @@ int print_sign(int n)
 		_putchar(48);
 		return (1);
 	}
-	else if (n < 0)
+	else (n < 0)
 	{
 		_putchar('-');
 	}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,28 +7,22 @@ void times_table(void)
 {
 int i, j, k;
 
-for (i = 0; i < 10; j++)
+for (i = 0; i <= 9; j++)
 {
-for (j = 0; j < 10; j++)
+	_putchar('0')
+for (j = 1; j <= 9; j++)
 {
-k = j * i;
-if (j == 0)
-{
-_putchar(k + '0');
-}
-}	if (k < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-} else if (k >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
+	_putchar(',')
+	_putchar('')
+
+	k = i * j;
+
+	if (k = <=9)
+		_putchar('');
+	else
+		-putchar((k / 10) + '0');
+	_putchar((k % 10) + '0');
 }
 _putchar('\n');
+}
 }

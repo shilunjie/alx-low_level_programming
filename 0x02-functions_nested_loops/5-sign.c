58661f91bea77197ae4993f5326640521c95dbd2
Shilunjie <lshilunji@gmail.com>
2023-06-20 17:44:52 -0700

    files changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,88 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and print + if n is graeter than zero
- * 0 and points 0 if n is zero
- * -1 and prints, if n is less than zero
- */
-int print_sign(int n)
+* print_times_table - prints the n times table, starting with 0
+* @n: number of the times table
+*/
+
+void print_times_table(int n)
+
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar(48);
-		return (1);
-	}
-	else (n < 0)
-	{
-		_putchar('-');
-	}
-		return (-1);
+
+int i, j, k;
+
+
+
+if (n >= 0 && n <= 15)
+
+{
+
+for (i = 0; i <= n; i++)
+
+{
+
+for (j = 0; j <= n; j++)
+
+{
+
+k = j * i;
+
+if (j == 0)
+
+{
+
+_putchar(k + '0');
+
+} else if (k < 10 && j != 0)
+
+{
+
+_putchar(',');
+
+_putchar(' ');
+
+_putchar(' ');
+
+_putchar(' ');
+
+_putchar(k + '0');
+
+} else if (k >= 10 && k < 100)
+
+{
+
+_putchar(',');
+
+_putchar(' ');
+
+_putchar(' ');
+
+_putchar((k / 10) + '0');
+
+_putchar((k % 10) + '0');
+
+} else if (k >= 100)
+
+{
+
+_putchar(',');
+
+_putchar(' ');
+
+_putchar((k / 100) + '0');
+
+_putchar(((k / 10) % 10) + '0');
+
+_putchar((k % 10) + '0');
+
+}
+
+}
+
+_putchar('\n');
+
+}
+
+}
+
 }
